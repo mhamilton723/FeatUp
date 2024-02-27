1d750f821f6bcb0ca8c4e1f8cf5085b18e54fca3
Mark Hamilton <marhamil@microsoft.com>
2024-02-27 14:16:05 -0500

    fix publishing

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
     name='featup',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'torch',
